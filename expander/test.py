6f096a0100fbb0980dbc34d413459c72f6d25c7a
Chelsea <chelseakct@gmail.com>
2017-04-05 20:36:09 +0000

    expander demo working

--- expander/test.py
+++ expander/test.py
@@ -1,6 +1,6 @@
 from Adafruit_MCP230xx import *
 
-mcp = Adafruit_MCP230xx(address = 0x20, num_gpios = 16) # MCP23017
+mcp = Adafruit_MCP230XX(address = 0x20, num_gpios = 16) # MCP23017
 
 # Set pins 0, 1 and 2 to output (you can set pins 0..15 this way)
 mcp.config(0, mcp.OUTPUT)
