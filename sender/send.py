55aeef9e4620af8fe0bb64c56aa68b3be557d17f
Chelsea Kwong <chelseakct@gmail.com>
2017-04-25 19:56:57 -0400

    added prelim calibration, cleaned up send.py

--- sender/send.py
+++ sender/send.py
@@ -1,40 +1,84 @@
+from Adafruit_MCP230xx import *
 import socket
 import RPi.GPIO as GPIO, time, os
-GPIO.setmode(GPIO.BCM)
+
+"""
+Global variables
+"""
 UDP_IP = "128.237.177.230" #receiver's IP
 UDP_Port = 5005
-MSG = 'heloo'
-# DEFAULT_VALUES_DICT = dict([('A', [0,0,0,0,0]),
-#                         ('B', [0,0,0,0,0]),
-#                         ('C', [0,0,0,0,0]),
-#                         ('D', [0,0,0,0,0]),
-#                         ('E', [0,0,0,0,0]),
-#                         ('F', [0,0,0,0,0]),
-#                         ('G', [0,0,0,0,0]),
-#                         ('H', [0,0,0,0,0])])
+mcp1 = Adafruit_MCP230XX(address = 0x20, num_gpios = 16) # MCP23017
+mcp2 = Adafruit_MCP230XX(address = 0x21, num_gpios = 16) # MCP23017
+DEFAULT_VALUES = dict() # default values of sensors when untouched
+THRESHOLD_VALUES = dict() # threshold difference to activate actuator
 
-DEFAULT_VALUES_DICT = dict( [ ('A',0), ('B', 0), ('C',0), ('D',0)])
-
-def RCtime(RCpin):
+"""
+Read from individual expander pin
+"""
+def RCtime(mcp, RCpin):
     reading = 0
-    GPIO.setup(RCpin, GPIO.OUT)
-    GPIO.output(RCpin, GPIO.LOW)
+    mcp.config(RCpin, mcp.OUTPUT)
+    GPIO.output(RCpin, 0)
     time.sleep(0.05)
 
-    GPIO.setup(RCpin, GPIO.IN)
-    while(GPIO.input(RCpin) == GPIO.LOW):
+    mcp.config(RCpin, mcp.INPUT)
+    while(mcp.input(RCpin) == 0):
         reading +=1
     return reading
 
+"""
+Go through all sensors and grab the default vals
+TODO: fine tune threshold values and min nums when pressed
+"""
+def calibrate():
+    # mcp 1: pins 0 thru 15
+    # mcp2: pins 0 thru 3
+    mcp = mcp1
+    currSensor = ord('A')
+    for x in range(0, 20):
+        loopCount = 100
+        tot = 0
+        count = 0
+        pinNum = x
+        if (x >= 16):
+            # use mcp 2 when done with 16 pins on mcp1
+            mcp = mcp2
+            pinNum = x % 16
+        while (count < loopCount):
+            tot += RCtime(mcp, pinNum)
+            count ++
+        avg = tot/loopCount
+        DEFAULT_VALUES[chr(currSensor)] = avg
+        THRESHOLD_VALUES[chr(currSensor)] = 8 # TODO: fine tune this
+        currSensor ++
+
+"""
+Read from expanders and consolidate into dictionary
+"""
 def sense():
-	#read from gpio headers and consolidate into dictionary
 	data = dict()
-	data['A'] = RCtime(24)
-    data['B'] = RCtime(23)
-    data['C'] = RCtime(25)
-    data['D'] = RCtime(22)
+    mcp = mcp1
+    currSensor = ord('A')
+    for x in range(0,20):
+        pinNum = x
+        if (x >= 16):
+            mcp = mcp2
+            pinNum = x % 16
+        reading = RCtime(pinNum)
+        diff = DEFAULT_VALUES[chr(currSensor)] - reading
+        data[chr(currSensor)] =  0
+        if (diff >= THRESHOLD_VALUES[chr(currSensor)]):
+            # touched, scale to 100
+            lowest = 8 #TODO: fine tune this, different lowest for different sensors?
+            range = DEFAULT_VALUES[chr(currSensor)] - THRESHOLD_VALUES[chr(currSensor)] - lowest
+            scaleFactor = 100/range
+            data[chr(currSensor)] =  scaleFactor * (reading-lowest)
+        currSensor ++
     return data
 
+"""
+calibrate and initiate connection, send data over
+"""
 def main():
 	calibrate()
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
